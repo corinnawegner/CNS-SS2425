74d474eebe95b14d16c367802e5fa8e87611020a
Corinna Wegner <corinna.wegner@outlook.de>
2024-11-18 16:42:38 +0100

    Plots for Iso rule. They look the same

--- Assignment 2/2_1_wegner.py
+++ Assignment 2/2_1_wegner.py
@@ -1,24 +1,54 @@
-from learning_rules import Neuron, Synapse
+from learning_rules import Neuron, Synapse, double_exponential
 import numpy as np
+import matplotlib.pyplot as plt
+
+time_de = np.linspace(0,2, 1000)
+list_de = [double_exponential(t) for t in time_de]
+plt.plot(time_de, list_de)
+plt.xlabel('Time')
+plt.ylabel('double exponential')
+plt.title("Double exponential function")
+plt.show()
 
 dt = 0.001
 learning_rate = 0.01
+tmax = 10
 
-neuron_1 = Neuron(5)
-neuron_2 = Neuron(5) # not learning
+neuron_1 = Neuron(5, total_spiking_time = tmax)
+neuron_2 = Neuron(5, total_spiking_time = 4, delay = -0.004) # not learning, Spike train with d = -40 ms
 post_neuron = Neuron(0)
 synapse_1 = Synapse(neuron_1, post_neuron, learning_rate)
 synapse_2 = Synapse(neuron_2, post_neuron, learning_rate, weight=1)
 
-neuron_1_train = neuron_1.generate_spike_train(10, 0.001)
-neuron_2_train_1 = neuron_2.generate_spike_train(4, 0.001, delay=-0.004) # Spike train with d = -40 ms
-neuron_2_train_2 = neuron_2.generate_spike_train(4, 0.001, delay=0.004) # Spike train with d = 40 ms
-
-list_t = np.arange(0, 10, 0.001)
+list_t = np.linspace(0, tmax, int(tmax/dt))
 list_weight = []
 
 for t in list_t:
-
     list_weight.append(synapse_1.weight)
     post_neuron.update_rate([synapse_1.weight, synapse_2.weight], [neuron_1.rate, neuron_2.rate])
-    synapse_1.update_weight()
+    synapse_1.update_weight(learning_rule='iso', delta_t=dt, t=t)
+
+plt.plot(list_t, list_weight)
+plt.title("Weight change for Synapse 1 with delay -0.004")
+plt.xlabel("Time")
+plt.ylabel("Weight")
+plt.show()
+
+neuron_3 = Neuron(5, total_spiking_time = tmax)
+neuron_4 = Neuron(5, total_spiking_time = 4, delay = 0.004) # not learning, Spike train with d = -40 ms
+post_neuron_2 = Neuron(0)
+synapse_3 = Synapse(neuron_3, post_neuron_2, learning_rate)
+synapse_4 = Synapse(neuron_4, post_neuron_2, learning_rate, weight=1)
+
+list_weight = []
+
+for t in list_t:
+    list_weight.append(synapse_3.weight)
+    post_neuron_2.update_rate([synapse_3.weight, synapse_4.weight], [neuron_3.rate, neuron_4.rate])
+    synapse_3.update_weight(learning_rule='iso', delta_t=dt, t=t)
+
+plt.plot(list_t, list_weight)
+plt.title("Weight change for Synapse 1 with delay 0.004")
+plt.xlabel("Time")
+plt.ylabel("Weight")
+plt.show()
--- learning_rules.py
+++ learning_rules.py
@@ -1,29 +1,36 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 class Neuron:
-    def __init__(self, constant_current):
+    def __init__(self, constant_current, mode = "periodic", total_spiking_time = 10, delay = 0):
         self.constant_current = constant_current
         self.rate = constant_current
+        # Needed for ISO rule, generated via generate_spike_train():
+        self.spike_train = None
+        self.convolved_spike_train = None
+        self.derived_spike_train = None
+        self.mode = mode
+        self.total_spiking_time = total_spiking_time
+        self.delay = delay
 
     def update_rate(self, presynaptic_weights, input_rates):
         self.rate = np.sum(np.array(presynaptic_weights)*np.array(input_rates)) + self.constant_current
 
-    def generate_spike_train(self, total_time, delta_t, delay = 0, mode = "periodic"):
+    def generate_spike_train(self, delta_t):
         spike_train = []
-        for i in range(0, total_time, delta_t):
-            if mode == "periodic":
-                # Calculate the interval between spikes in terms of steps
-                spike_interval = int(1 / self.rate / delta_t)  # interval in number of steps
+        if self.mode == "periodic":
+            # Calculate the interval between spikes in terms of steps
+            spike_interval = int(1 / self.rate / delta_t)  # interval in number of steps
 
-                for i in range(0, total_time, delta_t):
-                    if (i // delta_t) % spike_interval + delay == 0:  # Check if it's time for a spike
-                        spike_train.append(1)
-                    else:
-                        spike_train.append(0)
-
-        return spike_train
-
-
+            for i in np.arange(0, self.total_spiking_time, delta_t):
+                if (i // delta_t) % spike_interval + self.delay == 0:  # Check if it's time for a spike
+                    spike_train.append(1)
+                else:
+                    spike_train.append(0)
+        self.spike_train = np.array(spike_train)
+        input_trace = np.array([double_exponential(t) for t in np.linspace(0, 2, int(2/delta_t))])
+        self.convolved_spike_train = np.convolve(self.spike_train, input_trace)
+        self.derived_spike_train = np.gradient(self.spike_train)
 
 class Synapse:
     def __init__(self, in_neuron, out_neuron, learning_rate, weight=0):
@@ -33,8 +40,10 @@ class Synapse:
         self.out_neuron = out_neuron
         self.mean_rate_in = 0 # Only needed for Covariance rule
         self.mean_rate_out = 0
+        #self.convolved_input = self.in_neuron.spike_train
+        self.output_spike_train = self.out_neuron.spike_train
 
-    def update_weight(self, learning_rule="hebb", delta_t=0.1):
+    def update_weight(self, learning_rule="hebb", delta_t=0.1, t=0):
         rate_in = self.in_neuron.rate
         rate_out = self.out_neuron.rate
         if learning_rule == "hebb":
@@ -45,6 +54,8 @@ class Synapse:
             self.weight += self.oja_rule(rate_in, rate_out, delta_t)
         elif learning_rule == "cov":
             self.weight += self.covariance_rule(rate_in, rate_out, delta_t)
+        elif learning_rule == "iso":
+            self.weight += self.iso_rule(t, delta_t)
         else:
             raise ValueError(f"Unknown learning rule: {learning_rule}")
 
@@ -73,19 +84,33 @@ class Synapse:
         #print(mean_rate_in_old, mean_rate_out_old, rate_i, rate_j, delta_weight, self.out_neuron.rate, self.in_neuron.rate)
         return delta_weight
 
-    def iso_rule(self, input_spike_train, output_spike_train, t, delta_t):
-        H = 0.2
-        alpha = 1/20 * 10**2
-        beta = 1/2 * 10**2
-        h = lambda t: H * (np.exp(-alpha*t)-np.exp(-beta*t)) if t > 0 else 0
+    def iso_rule(self, t, delta_t):
+        if self.in_neuron.convolved_spike_train is None:
+            self.in_neuron.generate_spike_train(delta_t)
+        if self.out_neuron.derived_spike_train is None:
+            self.out_neuron.generate_spike_train(delta_t)
+        u = self.in_neuron.convolved_spike_train
+        dv = self.out_neuron.derived_spike_train
 
-        t_tot = len(input_spike_train)/delta_t
+        t_int = int(t/delta_t) - 1
+        print("t_int", t_int)
 
-        h_sequence = [h(t) for t in range(0, t_tot, delta_t)]
-        u = np.convolve(input_spike_train, h_sequence)
-        h_out = np.convolve(output_spike_train, h_sequence) #Todo: is this necessary?
-
-        dv = np.gradient(h_out)
-
-        delta_weight = self.learning_rate * dv[t*delta_t] * u[t*delta_t] * delta_t
+        delta_weight = self.learning_rate * dv[t_int] * u[t_int] * delta_t
         return delta_weight
+
+def double_exponential(t):
+    H = 0.2
+    alpha = 1 / 20 * 10 ** 2
+    beta = 1 / 2 * 10 ** 2
+    #h_sequence = [double_exponential(t) for t in np.arange(0, 10, 100)]
+    return H * (np.exp(-alpha*t)-np.exp(-beta*t)) if t > 0 else 0#, h_sequence
+
+"""
+time = np.linspace(0,2, 1000)
+
+list_de = [double_exponential(t) for t in time]
+
+print(time,list_de)
+plt.plot(time, list_de)
+plt.show()
+"""
