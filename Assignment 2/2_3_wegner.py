b9147cf2461aefcf4e8fad0a598ffaf42b22752f
Corinna Wegner <corinna.wegner@outlook.de>
2024-12-01 21:43:19 +0100

    2.3

--- Assignment 2/2_3_wegner.py
+++ Assignment 2/2_3_wegner.py
@@ -17,48 +17,33 @@ dt = 0.001
 learning_rate = 0.01
 tmax = 2
 
-neuron_1 = Neuron(5, total_spiking_time = tmax, mode="single")
-neuron_2 = Neuron(5, total_spiking_time = tmax, mode="single", delay = -0.1) # not learning, Spike train with d = -100 ms
-post_neuron = Neuron(0)
-synapse_1 = Synapse(neuron_1, post_neuron, learning_rate)
-synapse_2 = Synapse(neuron_2, post_neuron, learning_rate, weight=1)
+list_d = np.linspace(-0.1,0.1, 20)
+list_weights = []
 
-neuron_1.generate_spike_train(dt)
-neuron_2.generate_spike_train(dt)
+for d in list_d:
 
-list_t = np.linspace(0, tmax, int(tmax/dt))
-list_weight = []
+    neuron_1 = Neuron(5, total_spiking_time = tmax)
+    neuron_2 = Neuron(5, total_spiking_time = tmax, delay = d)
+    post_neuron = Neuron(0)
+    synapse_1 = Synapse(neuron_1, post_neuron, learning_rate)
+    synapse_2 = Synapse(neuron_2, post_neuron, learning_rate, weight=1)
 
-for t in list_t:
-    list_weight.append(synapse_1.weight)
-    post_neuron.generate_output_spike_train([synapse_1.weight, synapse_2.weight], [neuron_1.convolved_spike_train, neuron_2.convolved_spike_train])
-    synapse_1.update_weight(learning_rule='iso', delta_t=dt, t=t)
+    neuron_1.generate_spike_train(dt)
+    neuron_2.generate_spike_train(dt)
 
-plt.plot(list_t, list_weight)
-plt.title("Weight change for Synapse 1 with delay -0.1")
-plt.xlabel("Time")
+    list_t = np.linspace(0, tmax, int(tmax/dt))
+    list_weight = []
+
+    for t in list_t:
+        list_weight.append(synapse_1.weight)
+        post_neuron.generate_output_spike_train([synapse_1.weight, synapse_2.weight], [neuron_1.convolved_spike_train, neuron_2.convolved_spike_train])
+        synapse_1.update_weight(learning_rule='iso', delta_t=dt, t=t)
+
+    weight_final = synapse_1.weight
+    list_weights.append(weight_final)
+
+plt.plot(list_d, list_weights)
+plt.title("Final weight change curve")
+plt.xlabel("Delay d")
 plt.ylabel("Weight")
 plt.show()
-
-neuron_3 = Neuron(5, total_spiking_time = tmax, mode = "single")
-neuron_4 = Neuron(5, total_spiking_time = tmax, mode = "single", delay = 0.1) # not learning, Spike train with d = -40 ms
-post_neuron_2 = Neuron(0)
-synapse_3 = Synapse(neuron_3, post_neuron_2, learning_rate)
-synapse_4 = Synapse(neuron_4, post_neuron_2, learning_rate, weight=1)
-
-neuron_3.generate_spike_train(dt)
-neuron_4.generate_spike_train(dt)
-
-list_weight = []
-
-for t in list_t:
-    list_weight.append(synapse_3.weight)
-    post_neuron_2.generate_output_spike_train([synapse_3.weight, synapse_4.weight], [neuron_3.convolved_spike_train, neuron_4.convolved_spike_train])
-    synapse_3.update_weight(learning_rule='iso', delta_t=dt, t=t)
-
-plt.plot(list_t, list_weight)
-plt.title("Weight change for Synapse 1 with delay 0.1")
-plt.xlabel("Time")
-plt.ylabel("Weight")
-plt.show()
-
