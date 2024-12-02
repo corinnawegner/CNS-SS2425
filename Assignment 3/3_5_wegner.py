7c13fac079cd0c2cdc4444577da2b6a58ff91e00
Corinna Wegner <corinna.wegner@outlook.de>
2024-12-03 00:00:28 +0100

    State Space Influence

--- Assignment 3/3_5_wegner.py
+++ Assignment 3/3_5_wegner.py
@@ -7,21 +7,23 @@ from RL_environment import *
 def exponential_decay(n, A, B, lam):
     return A*np.exp(-n/lam) + B
 
-list_epsilon = [0, 0.2, 0.4, 0.6, 0.8]
+
+
+list_size = [5, 10, 20, 40, 80]
 list_avg_paths = []
 list_fit_params = []
 
 n_epochs = np.arange(1000)
 
-for eps in tqdm(list_epsilon, desc="Processing epsilon values"):
-    RL_env = create_environment(21, 21)
+for size in tqdm(list_size, desc="Processing size values"):
+    RL_env = create_environment(size, size)
     list_path_length = []
     for i in range(100):
         list_path_length_i = []
         for epoch in n_epochs:
             RL_env.back_to_start_position()
             while tuple(RL_env.pos) != tuple(RL_env.goal_state):
-                RL_env.epsilon_step(eps)
+                RL_env.epsilon_step(0.2)
                 RL_env.update_value(RL_env.positions[-2]) #We update the state before the current step
             list_path_length_i.append(len(RL_env.positions))
         list_path_length.append(list_path_length_i)
@@ -32,22 +34,22 @@ for eps in tqdm(list_epsilon, desc="Processing epsilon values"):
     list_avg_paths.append(mean_path_length)
     list_fit_params.append(popt)
 
-for eps, path, popt in zip(list_epsilon, list_avg_paths, list_fit_params):
-    plt.plot(n_epochs, path, label = f"Average path lengths for epsilon = {eps}")
-    plt.plot(n_epochs, exponential_decay(n_epochs, *popt), label=f"Fitted Function for epsilon = {eps}")
+for eps, path, popt in zip(list_size, list_avg_paths, list_fit_params):
+    plt.plot(n_epochs, path, label = f"Average path lengths for environment size = {size}")
+    plt.plot(n_epochs, exponential_decay(n_epochs, *popt), label=f"Fitted Function for environment size = {size}")
 plt.xlabel("epoch")
 plt.ylabel("Path length")
 plt.title("Average path lengths over 100 trials")
 plt.legend()
 plt.show()
 
-plt.plot(list_epsilon, list_fit_params[:,1])
+plt.plot(list_size, list_fit_params[:,1])
 plt.title("Dependency of parameter B on epsilon")
 plt.xlabel("epsilon")
 plt.ylabel("fit parameter")
 plt.show()
 
-plt.plot(list_epsilon, list_fit_params[:,2])
+plt.plot(list_size, list_fit_params[:,2])
 plt.title("Dependency of parameter Lambda on epsilon")
 plt.xlabel("epsilon")
 plt.ylabel("fit parameter")
