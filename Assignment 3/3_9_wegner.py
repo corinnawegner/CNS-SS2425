696f2961f2d6c1b3df9c19e46b4da44bd61f8c0d
Corinna Wegner <corinna.wegner@outlook.de>
2025-01-07 21:12:17 +0100

    All epsilon values in Actor-Critic

--- Assignment 3/3_9_wegner.py
+++ Assignment 3/3_9_wegner.py
@@ -1,6 +1,8 @@
 from scipy.optimize import curve_fit
 from RL_environment import *
+from tqdm import tqdm
 
+RL_env = create_environment(21, 21, learning_mode = "Actor-Critic")
 
 list_epsilon = [0, 0.2, 0.4, 0.6, 0.8]
 list_avg_paths = []
@@ -8,30 +10,44 @@ list_fit_params = []
 
 n_epochs = np.arange(1000)
 
-eps = 0.2
+for eps in tqdm(list_epsilon, desc="Processing epsilon values"):
+    RL_env = create_environment(21, 21)
+    list_path_length = []
+    for i in range(100):
+        list_path_length_i = []
+        for epoch in n_epochs:
+            RL_env.back_to_start_position()
+            while tuple(RL_env.pos) != tuple(RL_env.goal_state):
+                RL_env.softmax_step(eps)
+                RL_env.update_value(RL_env.positions[-2])  # We update the state before the current step
+            list_path_length_i.append(len(RL_env.positions))
+        list_path_length.append(list_path_length_i)
 
-RL_env = create_environment(21, 21, learning_mode = "Actor-Critic")
-list_path_length = []
-for i in range(100):
-    list_path_length_i = []
-    for epoch in n_epochs:
-        RL_env.back_to_start_position()
-        while tuple(RL_env.pos) != tuple(RL_env.goal_state):
-            RL_env.softmax_step(eps)
-            RL_env.update_value(RL_env.positions[-2]) #We update the state before the current step
-        list_path_length_i.append(len(RL_env.positions))
-    list_path_length.append(list_path_length_i)
+    mean_path_length = np.mean(list_path_length, axis=0)
+    popt, _ = curve_fit(exponential_decay, n_epochs, mean_path_length, p0=(1, 100, 1))
 
-mean_path_length = np.mean(list_path_length, axis=0)
-popt, _ = curve_fit(exponential_decay, n_epochs, mean_path_length, p0=(1, 100, 1))
+    list_avg_paths.append(mean_path_length)
+    list_fit_params.append(popt)
 
-list_avg_paths.append(mean_path_length)
-list_fit_params.append(popt)
-
-plt.plot(n_epochs, list_avg_paths[0], label = f"Average path lengths for epsilon = {eps}")
-plt.plot(n_epochs, exponential_decay(n_epochs, *popt), label=f"Fitted Function for epsilon = {eps}")
+for eps, path, popt in zip(list_epsilon, list_avg_paths, list_fit_params):
+    plt.plot(n_epochs, path, label = f"Average path lengths for epsilon = {eps}")
+    plt.plot(n_epochs, exponential_decay(n_epochs, *popt), label=f"Fitted Function for epsilon = {eps}")
 plt.xlabel("epoch")
 plt.ylabel("Path length")
 plt.title("Average path lengths over 100 trials")
 plt.legend()
 plt.show()
+
+plt.plot(list_epsilon, list_fit_params[:,1])
+plt.title("Dependency of parameter B on epsilon")
+plt.xlabel("epsilon")
+plt.ylabel("fit parameter")
+plt.show()
+
+plt.plot(list_epsilon, list_fit_params[:,2])
+plt.title("Dependency of parameter Lambda on epsilon")
+plt.xlabel("epsilon")
+plt.ylabel("fit parameter")
+plt.show()
+
+print(list_fit_params)
