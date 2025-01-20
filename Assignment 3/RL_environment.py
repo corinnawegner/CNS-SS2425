e73be36e8947d6f756306753a227a17dd88487ac
Corinna Wegner <corinna.wegner@outlook.de>
2025-01-20 11:59:07 +0100

    handling overflow values in softmax

--- Assignment 3/RL_environment.py
+++ Assignment 3/RL_environment.py
@@ -66,7 +66,7 @@ class RL_environment():
 
     def softmax_step(self, epsilon, wait = False):
         if epsilon == 1 or epsilon == 0:
-            self.epsilon_step(epsilon, wait=wait)
+            self.epsilon_step(epsilon)
             return 0
 
         actions = ["u", "d", "r", "l"]
@@ -78,12 +78,11 @@ class RL_environment():
         else:
             t = temperature(epsilon)
 
-            #action_to_index = {"u": 0, "d": 1, "r": 2, "l": 3}
             weights = {}
-            normalization_constant = 0
             for action in actions:
                 q = self.values[self.pos[0], self.pos[1], self.action_to_index(action)]
-                #print(f'q, t: ', q, t, q/t)
+                if q > 700*t:
+                    q = 700*t
                 weight_factor = np.exp(q/t)
                 if weight_factor < 1e-5:
                     weights[action] = 0
@@ -94,18 +93,13 @@ class RL_environment():
 
             # Normalize weights to probabilities
             probabilities = {action: weight / np.sum(list(weights.values())) for action, weight in weights.items()}
-            #print(weights, probabilities)
-            #action = actions[np.argmax(probabilities)]
 
             # Select an action randomly with the computed probabilities
             actions_list = list(probabilities.keys())
             probabilities_list = list(probabilities.values())
             action = np.random.choice(actions_list, p=probabilities_list)
 
-        if not wait:
-            self.action_to_step(action)  # immediately move
-
-        self.action_hist.append(action)
+        self.do_action(action)
 
     def update_value(self, position, next_action = None):
         a = self.action_hist[-1]
@@ -119,10 +113,9 @@ class RL_environment():
             update_factor = self.reward_function[self.pos[0], self.pos[1]] + self.discount_rate * q_val_next - self.values[position[0], position[1], z]
             self.values[position[0], position[1], z] += self.learning_rate * update_factor
         elif self.mode == "Actor-Critic":
-            state_prev = position
-            delta_error = self.reward_function[self.pos[0], self.pos[1]] + self.gamma * self.values[self.pos[0], self.pos[1], 4] - self.values[state_prev[0], state_prev[1], 4]
+            delta_error = self.reward_function[self.pos[0], self.pos[1]] + self.gamma * self.values[self.pos[0], self.pos[1], 4] - self.values[position[0], position[1], 4]
             self.values[position[0], position[1], z] += self.learning_rate * delta_error
-            self.values[state_prev[0], state_prev[1], 4] += self.beta * delta_error
+            self.values[position[0], position[1], 4] += self.beta * delta_error
         else:
             raise ValueError("Invalid learning mode. Choose between Q-Learning, SARSA and Actor-Critic, where default is Q-Learning.")
 
