a86d8715c61587770dab07dbe8e23723391ffcbe
Corinna Wegner <corinna.wegner@outlook.de>
2025-02-04 12:15:23 +0100

    More learning rates and timescales tested

--- Assignment 2/2_1_wegner.py
+++ Assignment 2/2_1_wegner.py
@@ -51,7 +51,7 @@ for delay in [-0.04, 0.04]:
     plt.show()
 
 #Varying learning rate:
-list_mu = [0.05, 0.1, 0.2, 0.5]
+list_mu = [0.05, 0.07, 0.09, 0.1, 0.2]
 list_developments = []
 for lr in list_mu:
     # Defining the system
@@ -85,7 +85,7 @@ plt.show()
 
 
 #Varying dt
-list_dt = [0.001, 0.01, 0.1]
+list_dt = [0.001, 0.005, 0.01, 0.02, 0.1]
 list_developments = []
 for dt in list_dt:
     # Defining the system
